bdc3509cb9e96b4fabb507d4819bbd2ab6ae0998
Nayarett <nayarett.castillo@gmail.com>
2024-09-10 23:10:50 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MessageApp from './components/MessageApp.vue'
+</script>
+
+<template>
+  <MessageApp />
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
